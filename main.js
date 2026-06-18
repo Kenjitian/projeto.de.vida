13cf1fc7b2ba5543ff0a17b1c8125968d51a133d
Kenjitian <lucas.kenji.cardoso@escola.pr.gov.br>
2026-06-18 10:41:28 +0000

    Aula 5 e 6

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++) {
   botoes[i].onclick = function () {
-
-    for (let j = 0; j < botoes.length; j++) {
+   for (let j = 0; j < botoes.length; j++) {
       botoes[j].classList.remove("ativo");
       textos[j].classList.remove("ativo");
     }
@@ -15,7 +14,50 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2026-05-29T00:00:00");
-let tempoAtual = new Date();
+const tempoObjetivo1 = new Date("2026-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2026-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2026-07-30T00:00:00");
+const tempoObjetivo4 = new Date("2027-02-01T00:00:00");
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
+
+function calculaTempo(tempoObjetivo) {
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundos = Math.floor(tempoFinal / 1000);
+  let minutos = Math.floor(segundos / 60);
+  let horas = Math.floor(minutos / 60);
+  let dias = Math.floor(horas / 24);
+  
+  segundos = segundos % 60;
+  minutos = minutos % 60;
+  horas = horas % 24;
+  if (tempoFinal > 0) {
+  return (
+    dias +
+    " dias " +
+    horas +
+    " horas " +
+    minutos +
+    " minutos " +
+    segundos +
+    " segundos"
+  );
+} else {
+  return "Prazo Finalizado";
+}
+
+}
+function atualizaCronometro() {
+  for (let i = 0; i < contadores.length; i++) {
+    contadores[i].textContent = calculaTempo(tempos[i]);
+  }
+}
+
+function comecaCronometro() {
+  atualizaCronometro();
+  setInterval(atualizaCronometro, 1000);
+}
+
+//comecaCronometro();
